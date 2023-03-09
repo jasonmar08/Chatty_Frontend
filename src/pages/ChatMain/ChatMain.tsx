7f289acd0c1b9d3bf963d77c1f8a50df830e24c6
Jason Martinez <jasonmar08@gmail.com>
2023-03-09 02:55:02 -0500

    install prettier and set up Home and App boilerplate

---
+++ src/pages/ChatMain/ChatMain.tsx
@@ -0,0 +1,16 @@
+import './ChatMain.scss'
+
+export const ChatMain = () => {
+  return (
+    <div>
+      <h1>This is the skeleton for the Chat Main page</h1>
+      <ul>
+        <li>different components will be populated on the screen in three columns</li>
+        <li>left column will be contacts</li>
+        <li>middle column will be all chat conversations</li>
+        <li>right column will be the selected thread with all messages populated</li>
+        <li>if no chat thread is seleted, the right column will be a boilerplate with app logo</li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.scss'
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>This is Home</h1>
+      <ul>
+        <li>This page will contain register or login form</li>
+        <li>Tabs for Register and Login will populate the appropriate form</li>
+      </ul>
+    </div>
+  )
+}
